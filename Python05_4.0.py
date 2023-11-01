4ec4f407dcec7f6a37df220bfaa08456c849c7a9
BrianCCardoso <brianccardoso@usp.br>
2023-11-01 17:14:15 -0300

    Update Python05_4.0.py

---
+++ Python05_4.0.py
@@ -0,0 +1,6 @@
+fav_dict = {'Livro' : 'Use a cabeça com Python',
+             'Som' : 'Sem medo de ser feliz',
+             'Árvore' : 'Mangueira'}
+
+fav_thing = 'Árvore'
+print(fav_dict[fav_thing]) 
