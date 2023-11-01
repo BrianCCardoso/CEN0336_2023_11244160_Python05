6cff85ac262917f1f670ae40b5af8970043ea96c
BrianCCardoso <brianccardoso@usp.br>
2023-11-01 17:19:31 -0300

    Update Python05_9.0.py

---
+++ Python05_9.0.py
@@ -0,0 +1,7 @@
+fav_dict = {'Livro' : 'Use a cabeça com Python',
+             'Som' : 'Sem medo de ser feliz',
+             'Árvore' : 'Mangueira',
+             'Organismo' : 'Cachorro'}
+
+for i in fav_dict:
+    print(i,fav_dict[i], sep=': ')
