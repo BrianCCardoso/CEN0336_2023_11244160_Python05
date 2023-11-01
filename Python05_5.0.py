49512c54403009b5db77b2f2895853fae47472ba
BrianCCardoso <brianccardoso@usp.br>
2023-11-01 17:15:12 -0300

    Update Python05_5.0.py

---
+++ Python05_5.0.py
@@ -0,0 +1,7 @@
+fav_dict = {'Livro' : 'Use a cabeça com Python',
+             'Som' : 'Sem medo de ser feliz',
+             'Árvore' : 'Mangueira',
+             'Organismo' : 'Cachorro'}
+
+fav_thing = 'Organismo'
+print(fav_dict[fav_thing])
